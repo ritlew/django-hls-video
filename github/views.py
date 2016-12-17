4aa6cc155ce81ae2b1184b0d0bb5027c0c957e85
samuel <samuel@ritlew.com>
2016-12-17 16:38:32 -0500

    removed debugging

--- github/views.py
+++ github/views.py
@@ -7,5 +7,5 @@ import subprocess
 def pull(request):
     if request.method == "POST":
         subprocess.call("ssh-agent bash -c 'ssh-add /home4/square13/public_html/ritlew/code/ritlew/id_rsa; git pull'", shell=True)
-        return HttpResponse("thanks", status=200)
-    return HttpResponse("sorry")
+        return HttpResponse(status=200)
+    return HttpResponse(status=404)
