ae961ef2eb3196228c7967ecdbe3d1255ce1b5d4
samuel <samuel@ritlew.com>
2016-12-17 16:37:13 -0500

    debug off in production

--- ritlew/settings.py
+++ ritlew/settings.py
@@ -120,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+if os.environ.get('DJANGO_SETTINGS') == 'production':
+    from ritlew.production import *
