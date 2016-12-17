c7d25ddb70e15fa56518e08cf41c73c81b3738b1
samuel <samuel@ritlew.com>
2016-12-17 16:31:08 -0500

    csrf_exempt

--- github/views.py
+++ github/views.py
@@ -1,13 +1,11 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.views.decorators.csrf import csrf_exempt
 import subprocess
 
-# Create your views here.
 
-
+@csrf_exempt
 def pull(request):
     if request.method == "POST":
         subprocess.call("ssh-agent bash -c 'ssh-add /home4/square13/public_html/ritlew/code/ritlew/id_rsa; git pull'", shell=True)
         return HttpResponse("thanks", status=200)
-    return HttpResponse("sorry", status=404)
+    return HttpResponse("sorry")
