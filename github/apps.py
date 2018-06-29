9a908d92cc867c41a7257234cbf2ce0390823b70
Samuel <skullhead23@gmail.com>
2018-06-28 20:16:19 -0400

    delete github

--- github/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GithubConfig(AppConfig):
-    name = 'github'
--- github/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, include
-from django.contrib import admin
-
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.pull, name="pull")
-]
