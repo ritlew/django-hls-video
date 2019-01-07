3d05ad536c6e5bc4c6f51b795c6df78ae54c1df4
Samuel <skullhead23@gmail.com>
2019-01-06 21:30:18 -0500

    Update requirements file. Add .gitignore. Update .ico view to prevent debug server errors. Add fontawesome files for music component of website.

--- home/urls.py
+++ home/urls.py
@@ -1,14 +1,9 @@
 from django.conf.urls import url, include
-from django.contrib import admin
-from django.views.generic.base import RedirectView
-
 from . import views
 
-favicon_view = RedirectView.as_view(url='/static/home/image/favicon.ico', permanent=True)
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
-    url(r'^favicon\.ico$', favicon_view),
     url(r'^video/$', views.video, name="video"),
     url(r'^music/$', views.music, name="music"),
     url(r'^blog/', include("blog.urls")),
--- ritlew/urls.py
+++ ritlew/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('', include('home.urls')),
     path('admin/', admin.site.urls),
