624b0581fb46e011d1f9fd322bc16b2d528c93ca
Samuel <>
2019-01-06 20:40:56 -0500

    Update blog to allow blog creation and detail view

--- ritlew/urls.py
+++ ritlew/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import include
+from django.urls import path
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^', include('home.urls')),
-    url(r'^admin/', admin.site.urls),
-]
+    path('', include('home.urls')),
+    path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
